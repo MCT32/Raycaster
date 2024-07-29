83ec4f8dc3689a302b4657b01648a849b76fd564
MCT32 <32090502+MCT32@users.noreply.github.com>
2024-07-29 18:27:00 +1000

    line renderer but its broken

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,50 @@
 use std::{num::NonZeroU32, rc::Rc};
 
-use softbuffer::{Context, Surface};
-use winit::{application::ApplicationHandler, event::WindowEvent, event_loop::{ControlFlow, EventLoop}, window::Window};
+use euclid::{default::Point2D};
+use softbuffer::{Buffer, Context, Surface};
+use winit::{application::ApplicationHandler, dpi::PhysicalPosition, event::WindowEvent, event_loop::{ControlFlow, EventLoop}, raw_window_handle::{HasDisplayHandle, HasWindowHandle}, window::Window};
+
+fn draw_line<T: HasDisplayHandle + HasWindowHandle>(buffer: &mut Buffer<T, T>, width: u32, start: Point2D<u32>, end: Point2D<u32>, color: u32) {
+    let start: Point2D<i32> = start.cast();
+    let end: Point2D<i32> = end.cast();
+    let width = width as i32;
+
+    // horizontal
+    if start.y == end.y {
+        for i in 0..(end.x - start.x) {
+            buffer[(start.y * width + start.x + i) as usize] = color;
+        }
+    }
+    // vertical
+    else if start.x == end.x {
+        for i in 0..(end.y - start.y) {
+            buffer[((start.y + i) * width + start.x) as usize] = color;
+        }
+    }
+    // diagonal
+    else {
+        let dx = end.x - start.x;
+        let dy = end.y - start.y;
+
+        let slope = dy as f32 / dx as f32;
+
+        if dx.abs() >= dy.abs() {
+            for i in 0..dx.abs() {
+                buffer[((start.y + (if dx.is_negative() {-i} else {i} as f32 * slope) as i32) * width + start.x + if dx.is_negative() {-i} else {i}) as usize] = color;
+            }
+        } else {
+            for i in 0..dy.abs() {
+                buffer[((start.y + if dy.is_negative() {-i} else {i}) * width + start.x + (if dy.is_negative() {-i} else {i} as f32 / slope) as i32) as usize] = color;
+            }
+        }
+    }
+}
 
 #[derive(Default)]
 struct App {
     window: Option<Rc<Window>>,
     surface: Option<Surface<Rc<Window>, Rc<Window>>>,
+    pos: Point2D<u32>,
 }
 
 impl ApplicationHandler for App {
@@ -40,17 +78,26 @@ impl ApplicationHandler for App {
                 for index in 0..(width * height) {
                     let y = index / width;
                     let x = index % width;
-                    let red = x % 255;
-                    let green = y % 255;
-                    let blue = (x * y) % 255;
+                    let mut red = 0;
+                    let mut green = 0;
+                    let mut blue = 0;
 
                     buffer[index as usize] = blue | (green << 8) | (red << 16);
                 }
 
+                let color = 0 | (255 << 8) | (255 << 16);
+
+                draw_line(&mut buffer, width, Point2D::new(width / 2, height / 2), self.pos, color);
+
                 buffer.present().unwrap();
             },
+            WindowEvent::CursorMoved { device_id, position } => {
+                self.pos = Point2D::new(position.x as u32, position.y as u32);
+
+                self.window.as_mut().unwrap().request_redraw();
+            }
             WindowEvent::CloseRequested => {
-                event_loop.exit()
+                event_loop.exit();
             },
             _ => (),
         }
